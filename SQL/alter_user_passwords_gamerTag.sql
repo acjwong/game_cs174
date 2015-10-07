8d01309a032956a3d78e0efecb2e7cbcb21fbb73
brian8lam <brian8lam@gmail.com>
2015-10-07 13:03:31 -0700

    Revert "Added a new SQL Dump file"
    This reverts commit 40e38e6d115b388bdaed8dae830ee5f034dfecbb.

--- SQL/alter_user_passwords_gamerTag.sql
+++
@@ -1,4 +0,0 @@
-UPDATE `bruteforce`.`players` SET `email`='brandon.rossi@sjsu.edu', `password`='password1' WHERE `idPlayers`='1';
-UPDATE `bruteforce`.`players` SET `email`='brian.lam@sjsu.edu', `password`='password2' WHERE `idPlayers`='2';
-UPDATE `bruteforce`.`players` SET `email`='alexander.wong@sjsu.edu', `password`='password3' WHERE `idPlayers`='3';
-UPDATE `bruteforce`.`players` SET `email`='mike.phe@sjsu.edu', `password`='password4' WHERE `idPlayers`='4';
