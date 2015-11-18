1f2cd918ce1445970a94d24d9dbf42e234fe9c62
brian8lam <brian8lam@gmail.com>
2015-11-18 14:28:56 -0800

    Fixed Register and added ajax to create user

--- game_test/db_insert_new_user.php
+++ game_test/db_insert_new_user.php
@@ -1,18 +1,17 @@
 <?php
 	include "class_players.php";
-	
-	function insert()
-	{
 
-		try{
+	
+
+	try{
 			$connection = new PDO("mysql:host=localhost;dbname=bruteforce", "bruteforce", "password");
 	    	$connection->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 
-			$firstName = filter_input(INPUT_GET, "firstname"); 
-			$lastName = filter_input(INPUT_GET, "lastname"); 
-			$gamertag = filter_input(INPUT_GET, "newgametag");
-			$email = filter_input(INPUT_GET, "email");
-			$newPassword = filter_input(INPUT_GET, "newpassword");
+			$firstName = filter_input(INPUT_POST, "firstname"); 
+			$lastName = filter_input(INPUT_POST, "lastname"); 
+			$gamertag = filter_input(INPUT_POST, "gamertag");
+			$email = filter_input(INPUT_POST, "email");
+			$newPassword = filter_input(INPUT_POST, "password");
 
 
 			$query = "INSERT INTO Players(firstName, lastName, gamerTag, email, password) 
@@ -26,17 +25,16 @@
 		    $ps->bindParam(':password', $newPassword);
 		    $ps->execute();
 		    $ps->setFetchMode(PDO::FETCH_CLASS, "Players");
+
+		    if (!isset($_COOKIE['gamertag']))
+			{
+				setcookie('gamertag', $gamertag);
+			}
+
+		    echo 'true';
 		}
 		catch(Exception $e)
 		{
 			echo 'ERROR****: '.$e->getMessage();
 		}
-	}
-
-	
-	
-	insert();
-	header('Location: home.php');
-	exit;
-
 ?>
